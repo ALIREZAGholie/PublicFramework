67077b2a53672ca82c2b75f925f14f9578fcd7ad
Alireza Gholie <alireza.gholie@gmail.com>
2025-07-15 15:08:06 +0330

    System Error Edit

--- Webgostar.Framework.Base/BaseModels/SystemError.cs
+++ Webgostar.Framework.Base/BaseModels/SystemError.cs
@@ -2,10 +2,28 @@
 {
     public class SystemError : AggregateRoot
     {
-        public string? Data { get; set; }
+        public string? Data { get;  set; }
         public string PersianDate { get; set; }
-        public string Message { get; set; }
+        public string Message { get;  set; }
         public string? InnerExceptionMessage { get; set; }
         public string ErrorFile { get; set; }
+
+        public SystemError()
+        {
+            
+        }
+
+        public SystemError(string message)
+        {
+            Message = message;
+        }
+
+
+        public SystemError Edit(string message)
+        {
+            Message = message;
+
+            return this;
+        }
     }
 }
